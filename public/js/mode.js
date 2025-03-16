11a002f4aa3783f6b41e6fffd713563e17f460ff
Vu Do <vudo.contact.vn@gmail.com>
2025-03-16 21:09:57 +0700

    Enhance security features: prevent F12 and right-click actions, update footer to dynamically display the current year, and improve Noti component modal behavior.

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,1 +1,32 @@
-console.log("%cCode by @vudovn", "color: red; font-size: 40px;");
+(function () {
+  console.log("%cCode by @vudovn", "color: red; font-size: 40px;");
+  // Chặn F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+  document.addEventListener("keydown", function (event) {
+    if (
+      event.keyCode === 123 || // F12
+      (event.ctrlKey &&
+        event.shiftKey &&
+        (event.keyCode === 73 || event.keyCode === 74)) || // Ctrl+Shift+I/J
+      (event.ctrlKey && event.keyCode === 85) // Ctrl+U
+    ) {
+      event.preventDefault();
+    }
+  });
+
+  // Chặn chuột phải
+  document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+  });
+
+  // Phát hiện DevTools đang mở
+  setInterval(function () {
+    const before = new Date().getTime();
+    debugger;
+    const after = new Date().getTime();
+    if (after - before > 100) {
+      console.clear();
+      alert("Adu hacker!");
+      window.location.href = "/";
+    }
+  }, 1000);
+})();
--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -1,4 +1,10 @@
+"use client";
+import { useState, useEffect } from "react";
 function AppFooter() {
+  const [year, setYear] = useState(null);
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
   return (
     <div className="footer__custom">
       <footer className="mt-4 d-flex flex-wrap justify-content-between align-items-center p-3 border-top">
@@ -8,7 +14,7 @@ function AppFooter() {
             className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           ></a>
           <span className="mb-3 mb-md-0 text-dark fw ">
-            © {new Date().getFullYear()} code by{" "}
+            © {year ? year : "Loading..."} code by{" "}
             <a
               className="text-warning"
               href="http://facebook.com/vudovn.354"
