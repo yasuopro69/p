acf560a32ab104bd088ae5ffafa37816ef505a8d
yasuopro69 <152374779+yasuopro69@users.noreply.github.com>
2025-03-20 15:08:07 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,12 +17,12 @@ import "../assets/css/custom.css";
 export const metadata = {
   title: "Xem phim online miễn phí",
   description:
-    "Phimmoi ⚡ Phim HD VietSub | Phim Lẻ | Phim Bộ | Phim Chiếu Rạp | Xem phim Online miễn phí",
+    "Gin Movies ⚡ Phim HD VietSub | Phim Lẻ | Phim Bộ | Phim Chiếu Rạp | Xem phim Online miễn phí",
   imageUrl:
     "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-1/381271892_805928278202949_6388609339400536337_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YFb859k94L8Q7kNvgEyV2nL&_nc_ht=scontent.fsgn5-6.fna&oh=00_AfAxzB7t6FbhZPtOvgKvxggnMuv_greJ9Ge85i9sJVLKBw&oe=663DB2D2",
   url: "/",
   type: "website",
-  site_name: "Phimmoi-vudovn",
+  site_name: "Gin Movies",
   locale: "vi_VN",
 };
 
