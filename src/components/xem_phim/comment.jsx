0beb9c879687c977cf3a9490fec37d419d1d9819
yasuopro69 <152374779+yasuopro69@users.noreply.github.com>
2025-03-21 22:39:01 +0700

    Update comment.jsx

--- src/components/xem_phim/comment.jsx
+++ src/components/xem_phim/comment.jsx
@@ -1,51 +1,52 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 function Comment() {
-    const [isVisible, setIsVisible] = useState(false); // Set to false to hide by default
-    const url = window.location.href;
+  const [showComments, setShowComments] = useState(true);
+  const url = window.location.href;
 
-    useEffect(() => {
-        if (!isVisible) return; // Prevent script execution if comments are hidden
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "458546473274979",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v19.0",
+      });
+      window.FB.XFBML.parse();
+    };
 
-        window.fbAsyncInit = function () {
-            window.FB.init({
-                appId: "458546473274979",
-                autoLogAppEvents: true,
-                xfbml: true,
-                version: "v19.0",
-            });
-            window.FB.XFBML.parse();
-        };
+    (function (d, s, id) {
+      let js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/vi_VN/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
 
-        (function (d, s, id) {
-            var js,
-                fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/vi_VN/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, "script", "facebook-jssdk");
-    }, [isVisible]); // Only run when visibility changes
+  // When showComments is false, nothing is rendered.
+  if (!showComments) {
+    return null;
+  }
 
-    return (
-        <div>
-            <button onClick={() => setIsVisible(!isVisible)}>
-                {isVisible ? "Hide Comments" : "Show Comments"}
-            </button>
-            {isVisible && (
-                <div
-                    style={{ background: "#fff", borderRadius: "7px" }}
-                    className="fb-comments"
-                    data-href={url}
-                    data-numposts="5"
-                    data-width="100%"
-                />
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => setShowComments(false)}>
+        Delete Comments
+      </button>
+      <div
+        style={{ background: "#fff", borderRadius: "7px" }}
+        className="fb-comments"
+        data-href={url}
+        data-numposts="5"
+        data-width="100%"
+      />
+    </div>
+  );
 }
 
 export default Comment;
