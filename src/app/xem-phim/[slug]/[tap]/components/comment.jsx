d4a246eb65fd3134c01f0a553ff821c16b20bf58
yasuopro69 <152374779+yasuopro69@users.noreply.github.com>
2025-03-21 22:42:10 +0700

    Update comment.jsx

--- src/app/xem-phim/[slug]/[tap]/components/comment.jsx
+++ src/app/xem-phim/[slug]/[tap]/components/comment.jsx
@@ -2,28 +2,37 @@ import { useEffect, useState } from "react";
 
 function Comment() {
   const [url, setUrl] = useState("");
+  const [showComments, setShowComments] = useState(true); // state to control visibility
+
   useEffect(() => {
     setUrl(window.location.href);
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "458546473274979",
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: "v19.0",
-      });
-      window.FB.XFBML.parse();
-    };
+    if (showComments) {
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: "458546473274979",
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: "v19.0",
+        });
+        window.FB.XFBML.parse();
+      };
 
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/vi_VN/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
+      (function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/vi_VN/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    }
+  }, [showComments]);
+
+  // If showComments is false, return null (renders nothing)
+  if (!showComments) {
+    return null;
+  }
 
   return (
     <div
