c7c9e54fc448168bf5c271cf6e1dceffcb92ebd2
yasuopro69 <152374779+yasuopro69@users.noreply.github.com>
2025-03-21 21:59:31 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: "https://vudo.id.vn", // Đổi thành domain của bạn
-  generateRobotsTxt: true, // Tự động tạo robots.txt
+  siteUrl: "https://p.zefiq.com", // Đổi thành domain của bạn
+  generateRobotsTxt: false, // Tự động tạo robots.txt
 };
